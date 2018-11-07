04cdada6fd7aba8760eb303babc4e50a9a0ba5c6
Sebastian Utbult <sebbeprooo@gmail.com>
2018-11-07 20:40:53 +0100

    Upgift 1,2 gjorda

---
+++ src/Files/CallMethods_.java
@@ -0,0 +1,18 @@
+package Files;
+
+import java.util.Scanner;
+
+public class CallMethods_ {
+	
+	public static void main(String[] args) {
+		
+		Scanner jeff = new Scanner(System.in);
+		
+		//1
+		double volume = 80;
+		System.out.println("Upgift 1:");
+		System.out.println(Mylibrary.volumeToMass(Solidtable_.IRON, volume));
+		
+	}
+	
+}
